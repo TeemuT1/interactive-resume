db442c99b4b52c7bdb458aee8225948791b5a06c
Teemu <teemu_typpi@hotmail.com>
2020-08-05 22:35:13 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function()
+{
+	applyNavigation(); 
+	applyMailTo();
+	checkHash();
+});
+
+/* NAVIGATION FUNCTIONS */
+
+function applyNavigation()
+{
+    applyNavigationFixForPhone();
+	applyClickEvent();
+	applyScrollSpy();
+}
+
+function applyClickEvent()
+{
+	$('a[href*=#]').on('click', function(e)
+	{
+		e.preventDefault();
+		
+		if( $( $.attr(this, 'href') ).length > 0 )
+		{
+			$('html, body').animate(
+			{
+				scrollTop: $( $.attr(this, 'href') ).offset().top
+			}, 400);
+		}
+		return false;
+	});
+}
+
+function applyNavigationFixForPhone()
+{
+	$('.navbar li a').click(function(event) 
+	{
+		$('.navbar-collapse').removeClass('in').addClass('collapse');
+	});
+}
+
+function applyScrollSpy()
+{
+	$('#navbar-example').on('activate.bs.scrollspy', function() 
+	{
+		window.location.hash = $('.nav .active a').attr('href').replace('#', '#/');
+	});
+}
+
+/* MAILTO FUNCTION */
+
+function applyMailTo()
+{
+	$('a[href*=mailto]').on('click', function(e)
+	{
+		var lstrEmail = $(this).attr('href').replace('mailto:', '');
+		
+		lstrEmail = lstrEmail.split('').reverse().join('')
+		
+		$(this).attr('href', 'mailto:' + lstrEmail);
+	});
+}
+
+/* HASH FUNCTION */
+
+function checkHash()
+{
+	lstrHash = window.location.hash.replace('#/', '#');
+	
+	if($('a[href='+ lstrHash +']').length > 0)
+	{
+		$('a[href='+ lstrHash +']').trigger('click');
+	}
+}
